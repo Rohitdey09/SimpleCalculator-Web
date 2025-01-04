41cb20bf2d9be58cde34e2a14652a7c2d7fdf8f7
Rohit Dey <102870148+Rohitdey09@users.noreply.github.com>
2025-01-04 23:54:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-getqueryselector
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.num button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+        if (value === "AC") {
+            display.innerText = '0';
+        } else if (value === '=') {
+            try {
+                display.innerText = eval(display.innerText);
+            } catch (e) {
+                display.innerText = 'Error';
+            }
+        } else {
+            if (display.innerText === '0') {
+                display.innerText = value;
+            } else {
+                display.innerText += value;
+            }
+        }
+    });
+});
